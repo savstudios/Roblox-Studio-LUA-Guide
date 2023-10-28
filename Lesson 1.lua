6657d70ceccf30f4cdfa9864d01c7592b4663063
savstudios <149246059+savstudios@users.noreply.github.com>
2023-10-28 13:57:25 +0100

    Lesson 1.lua

---
+++ Lesson 1.lua
@@ -0,0 +1,35 @@
+-- 1a
+
+print(18+4)
+
+-- 1b
+
+name = "Bob"
+height = 1.5
+
+print(name.." is "..height.." m tall.")
+
+-- 1c
+
+print(8+4 .. 2-4)
+
+-- 1d
+
+name = "Ksawery"
+age = "11"
+birth = "Poland"
+
+print(name.." is "..age.." and was born in "..birth)
+
+-- 1e
+
+planet1 = "mercury,"
+planet2 = "venus,"
+planet3 = "earth,"
+planet4 = "mars,"
+planet5 = "jupiter,"
+planet6 = "saturn,"
+planet7 = "uranus,"
+planet8 = "neptune"
+
+print(planet1..planet2..planet3..planet4..planet5..planet6..planet7..planet8)
